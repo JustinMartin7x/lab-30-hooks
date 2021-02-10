3a310e5cd99602bd7341b62a6231f582b3169bac
JustinMartin7x <justin.martin7x@gmail.com>
2021-02-09 17:34:39 -0800

    added header

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import CharactersPage from '../../Containers/CharactersPages';
 import CharacterById from '../../Containers/CharacterById';
+import Header from '../Header/Header';
 
 
 export default function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={CharactersPage} />
-        <Route path="/characters/:id" component={CharacterById} />
-      </Switch>
-    </Router>
+    <>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={CharactersPage} />
+          <Route path="/characters/:id" component={CharacterById} />
+        </Switch>
+      </Router>
+    </>
   );
 }
